e7a35f957e85fb3e24b665e75b413eab9521a356
Yuri Faria <yuri.yurifaria@gmail.com>
2019-10-05 21:42:45 -0300

    bug fixed

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@ function getImageOfArticle(title) {
 
 function formateDate(date) {
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().length === 1 ? `0${date.getMonth()}` : date.getMonth();
+  const month = (date.getMonth() + 1).toString().length === 1 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
   const day = date.getDate().toString().length === 1 ? `0${date.getDate()}` : date.getDate();
   return `${year}${month}${day}`;
 }
